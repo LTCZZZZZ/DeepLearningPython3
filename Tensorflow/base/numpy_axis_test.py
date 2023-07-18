90336f2f622ab8dba8e49292a3192a04abf8e376
LTC <>
2023-07-18 11:08:57 +0800

    feat: numpy的axis最简单函数实现，slice对象，np.s_

--- Tensorflow/base/numpy_axis_test.py
+++ Tensorflow/base/numpy_axis_test.py
@@ -1,4 +1,11 @@
 # 看源码，好像是用C写了之后，在numpy/core/__init__.py文件中调用copyreg模块注册pickle实现的
+# 经验证，已经可以用如下python代码实现了，不需要再用exec了，但就速度来说，肯定是numpy内部实现的更快
+
+# 关键字：轴，axis
+
+# np.lib.index_tricks 有许多可以简化索引的函数(和类)
+# 可使用np.s_()函数来生成切片对象，或直接使用slice()函数
+
 import numpy as np
 
 
@@ -9,9 +16,15 @@ def self_sum(m, axis=None):
         s = m.shape
         l = []
         for i in range(s[axis]):
-            command = 'l.append(m[' + ':,' * axis + f'{i},' + ':,' * (len(s) - axis - 1) + '])'
+            # command = 'l.append(m[' + ':,' * axis + f'{i},' + ':,' * (len(s) - axis - 1) + '])'
             # print(command)
-            exec(command)  # 问：这种取值方式不用exec怎么实现呢？？见文件头部说明
+            # exec(command)  # 问：这种取值方式不用exec怎么实现呢？？见文件头部说明
+
+            # 直接实现
+            indexes = [slice(None)] * len(s)
+            indexes[axis] = i
+            l.append(m[tuple(indexes)])  # 注意这里必须是元组tuple，不能是list
+
         return sum(l)
 
 
@@ -34,8 +47,5 @@ print(np.sum(a, axis=2, keepdims=True))
 # np.add.reduce()
 print(np.sum(a, axis=(0, 1)))  # 这个功能我上面的函数没有实现，不过实现起来不难
 
-# string = '''for i in range(3):
-#     print(i)
-#     if i > 1:
-#         print("i > 1")'''
-# exec(string)
+s = np.s_[1:5:2,::3]
+print(s)
